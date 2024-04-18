58294d92e564b6516b48977ef397e80d0efaebed
BlueberryWeb <101658724+BlueberryWeb@users.noreply.github.com>
2024-04-18 11:39:05 -0600

    responsive corrections

--- src/modules/layout/PortalLayout.vue
+++ src/modules/layout/PortalLayout.vue
@@ -1,63 +1,69 @@
 <template>
-    <HeaderHome />
-    <SliderPortal />
-    <div class="area-scrollarea">
-        <router-view />
-        <FooterPortal/>
+  <HeaderHome />
+  <SliderPortal />
+  <div class="area-scrollarea">
+    <router-view />
+    <div class="mb-5">
+      <FooterPortal />
     </div>
+  </div>
 </template>
 
 <script>
-import { defineAsyncComponent } from '@vue/runtime-core'
+import { defineAsyncComponent } from "@vue/runtime-core";
 
 export default {
-    components: {
-        HeaderHome: defineAsyncComponent(() => import('../components/PortalHeader.vue')),
-        FooterPortal: defineAsyncComponent(() => import('../components/FooterPortal.vue')),
-    }
-}
+  components: {
+    HeaderHome: defineAsyncComponent(() =>
+      import("../components/PortalHeader.vue")
+    ),
+    FooterPortal: defineAsyncComponent(() =>
+      import("../components/FooterPortal.vue")
+    ),
+  },
+};
 </script>
 
 <style scoped lang="css">
-input[type="checkbox"]:checked~#sidebarMenu {
-    transform: translateX(0);
+input[type="checkbox"]:checked ~ #sidebarMenu {
+  transform: translateX(0);
 }
 
-input[type=checkbox] {
-    transition: all 0.3s;
-    box-sizing: border-box;
-    display: none;
+input[type="checkbox"] {
+  transition: all 0.3s;
+  box-sizing: border-box;
+  display: none;
 }
 
 .area-scrollarea {
-    height: calc(95.5vh - 40px);
-    overflow: scroll;
-    overflow-x: hidden;
-    
+  height: calc(95.5vh - 40px);
+  overflow: scroll;
+  overflow-x: hidden;
 }
 
 .area-scrollarea::-webkit-scrollbar {
-    -webkit-appearance: none;
+  -webkit-appearance: none;
 }
 
 .area-scrollarea::-webkit-scrollbar:vertical {
-    width:10px;
+  width: 10px;
 }
 
-.area-scrollarea::-webkit-scrollbar-button:increment,.area-scrollarea::-webkit-scrollbar-button {
-    display: none;
-} 
+.area-scrollarea::-webkit-scrollbar-button:increment,
+.area-scrollarea::-webkit-scrollbar-button {
+  display: none;
+}
 
 .area-scrollarea::-webkit-scrollbar:horizontal {
-    height: 10px;
+  height: 10px;
 }
 
 .area-scrollarea::-webkit-scrollbar-thumb {
-    background-color: #797979;
-    /* border-radius: 20px; */
+  background-color: #797979;
+  /* border-radius: 20px; */
 }
 
 .area-scrollarea::-webkit-scrollbar-track {
-    border-radius: 10px;  
+  border-radius: 10px;
 }
 </style>
